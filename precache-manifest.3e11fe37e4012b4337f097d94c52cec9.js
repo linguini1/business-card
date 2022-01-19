b666160757278250c0b6dc517e6791662dd2d343
linguini1 <matteo.golin@gmail.com>
2022-01-19 17:27:33 -0500

    Updates

---
+++ precache-manifest.3e11fe37e4012b4337f097d94c52cec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e4cc4a90c8527893446317b1671e24",
+    "url": "/business-card/index.html"
+  },
+  {
+    "revision": "f6ff6a163deb7dfbc062",
+    "url": "/business-card/static/css/main.7bbd1bdf.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/business-card/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "f6ff6a163deb7dfbc062",
+    "url": "/business-card/static/js/main.ee48acbc.chunk.js"
+  },
+  {
+    "revision": "9d57810a2ad39bb0c1b3",
+    "url": "/business-card/static/js/runtime~main.cec67399.js"
+  }
+]);
